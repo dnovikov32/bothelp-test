b8a2392e52739520141a0a2d8b915c7ec0fc89ae
dn <dn@t1000.su>
2020-10-26 15:33:15 +0300

    feat(queue): added script to filling the events queue

---
+++ src/generate_events.php
@@ -0,0 +1,38 @@
+<?php
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+use Predis\Client;
+
+const LIMIT_ACCOUNTS = 1000;
+const LIMIT_EVENTS = 10000;
+const LIMIT_ACCOUNT_EVENTS = 10;
+
+$redis = new Client([
+    'host' => 'redis',
+    'port' => 6379,
+]);
+
+$redis->del('queue:events');
+
+$eventsCount = 0;
+
+while ($eventsCount < LIMIT_EVENTS) {
+    $account = mt_rand(1, LIMIT_ACCOUNTS);
+
+    $eventsNumber = mt_rand(1, LIMIT_ACCOUNT_EVENTS);
+    $events = [];
+
+    for ($i = 1; $i <= $eventsNumber; $i++) {
+        $events[] = json_encode([
+            'accountId' => $account,
+            'eventId' => $i,
+        ]);
+    }
+
+    $redis->rpush('queue:events', $events);
+
+    $eventsCount += $eventsNumber;
+
+    echo sprintf("Account %d events %s\n", $account, $eventsNumber);
+}
