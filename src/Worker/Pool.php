874ba4f9808b35c41fb894c0fb6755161b1cab40
dnovikov <dn@t1000.su>
2020-10-30 09:29:41 +0300

    feat(pool): added check of tasks in the queue

--- src/Worker/Pool.php
+++ src/Worker/Pool.php
@@ -4,13 +4,42 @@ declare(strict_types=1);
 
 namespace App\Worker;
 
+use Predis\Client;
+
 class Pool
 {
+    /**
+     * Экземпляр клиента Redis.
+     */
+    private Client $redis;
+
+    /**
+     * Имя ключа очереди всех событий.
+     */
+    private string $queue;
+
+    public function __construct(Client $redis, string $queue)
+    {
+        $this->redis = $redis;
+        $this->queue = $queue;
+    }
+
+    public function hasJob(): bool
+    {
+        return (bool) $this->redis->llen($this->queue);
+    }
+
+    /**
+     * Возвращает количество активных процессов обработчиков.
+     */
     public function getWorkersCount(): int
     {
         return (int) exec('ps aux | grep -v grep | grep -c "worker.php"');
     }
 
+    /**
+     * Запускает новый процесс обработчик.
+     */
     public function runWorker(): void
     {
         $script = __DIR__ . '/../worker.php';
--- src/main.php
+++ src/main.php
@@ -4,15 +4,33 @@ declare(strict_types=1);
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
+use Predis\Client;
 use App\Worker\Pool;
 
-const LIMIT_WORKERS = 256;
+(new \Dotenv\Dotenv(__DIR__ . '/../'))->load();
 
-$pool = new Pool;
+$queue = getenv('EVENTS_QUEUE_KEY_NAME');
+$limitWorkers = getenv('LIMIT_WORKERS');
+
+$pool = new Pool(
+    new Client([
+        'host' => getenv('REDIS_HOST'),
+        'port' => getenv('REDIS_PORT'),
+    ]),
+    $queue
+);
 
 echo "Start main process\n";
 
-while ($pool->getWorkersCount() < LIMIT_WORKERS) {
-    echo sprintf("Workers count: %d\n", $pool->getWorkersCount());
-    $pool->runWorker();
+while (true) {
+    if (! $pool->hasJob()) {
+        echo "Waiting\n";
+        sleep(3);
+        continue;
+    }
+
+    if ($pool->getWorkersCount() < $limitWorkers) {
+        $pool->runWorker();
+        echo sprintf("Workers count: %d\n", $pool->getWorkersCount());
+    }
 }
